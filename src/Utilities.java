cf99c3b0035b41670b5e7db394287f453400d6a7
Jason Chris <rd.jasonchris@gmail.com>
2024-05-28 12:59:00 +0300

    Sprint3_ETU002562

--- src/Utilities.java
+++ src/Utilities.java
@@ -11,7 +11,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class Utilities {
-    public void initializeControllers(HttpServlet svr, List<String> controllerList,HashMap<String,Mapping> urlMethod) {
+    // Sprint 1 : show the url
+    public void initializeControllers(HttpServlet svr, List<String> controllerList,
+            HashMap<String, Mapping> urlMethod) {
         try {
             ServletContext context = svr.getServletContext();
             String packageName = context.getInitParameter("Controller");
@@ -23,7 +25,7 @@ public class Utilities {
                 URL resource = resources.nextElement();
                 if (resource.getProtocol().equals("file")) {
                     File file = new File(resource.toURI());
-                    scanControllers(file, packageName, controllerList,urlMethod);
+                    scanControllers(file, packageName, controllerList, urlMethod);
                 }
             }
 
@@ -32,7 +34,9 @@ public class Utilities {
         }
     }
 
-    public void scanControllers(File directory, String packageName, List<String> controllerList,HashMap<String,Mapping> urlMethod) {
+    // Sprint 2 : show Controller
+    public void scanControllers(File directory, String packageName, List<String> controllerList,
+            HashMap<String, Mapping> urlMethod) {
         if (!directory.exists()) {
             return;
         }
@@ -44,7 +48,7 @@ public class Utilities {
 
         for (File file : files) {
             if (file.isDirectory()) {
-                scanControllers(file, packageName + "." + file.getName(), controllerList,urlMethod);
+                scanControllers(file, packageName + "." + file.getName(), controllerList, urlMethod);
             } else if (file.getName().endsWith(".class")) {
                 String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
                 try {
@@ -52,12 +56,12 @@ public class Utilities {
                     if (clazz.isAnnotationPresent(AnnotationController.class)) {
                         controllerList.add(className);
                         Method[] methods = clazz.getDeclaredMethods();
-                        for(Method method : methods) {
-                            if(method.isAnnotationPresent(AnnotationMethode.class)){
+                        for (Method method : methods) {
+                            if (method.isAnnotationPresent(AnnotationMethode.class)) {
                                 AnnotationMethode annt = method.getAnnotation(AnnotationMethode.class);
                                 Mapping map = new Mapping();
-                                map.add(clazz.getName(),method.getName()); 
-                                urlMethod.put(annt.value(),map);
+                                map.add(clazz.getName(), method.getName());
+                                urlMethod.put(annt.value(), map);
                             }
                         }
                     }
@@ -71,15 +75,29 @@ public class Utilities {
     public String extractRelativePath(HttpServletRequest request) {
         String fullUrl = request.getRequestURL().toString(); // Obtenez l'URL complet
         String[] relativePath = fullUrl.split("/");// Supprimer le chemin de base de l'URL complet
-        return relativePath[relativePath.length -1];
+        return relativePath[relativePath.length - 1];
     }
 
-    public Mapping ifMethod(HttpServletRequest request,HashMap<String,Mapping> urlMethod){
+    public Mapping ifMethod(HttpServletRequest request, HashMap<String, Mapping> urlMethod) {
         String method = this.extractRelativePath(request);
-        if(urlMethod.containsKey(method)){
+        if (urlMethod.containsKey(method)) {
             return urlMethod.get(method);
         }
         return null;
     }
 
+    // Sprint 3 : call the method of the controller
+    public Object callMethod(Mapping mapping) throws Exception {
+        try {
+            // get the class
+            Class<?> clazz = Class.forName(mapping.getKey());
+
+            // Class method
+            Object obj = clazz.getDeclaredConstructor().newInstance();
+            Method method = clazz.getMethod(mapping.getValue().trim());
+            return (Object) method.invoke(obj);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
