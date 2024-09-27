ea43f39b4e0cf895e95486d1610120edb85fde0d
Jason Chris <rd.jasonchris@gmail.com>
2024-09-27 16:24:16 +0300

    sprint 9

--- compile.bat
+++ compile.bat
@@ -9,7 +9,7 @@ REM Création du fichier JAR
 jar cvf %JAR_FILE% -C %CLASSES_DIR% .
 
 REM couper le fichier .jar vers lib 
-xcopy /s /q /y "Framework.jar" "D:\itu\S4\Mr_Naina\Nouveau dossier\Nouveau dossier\SprintTest2\lib"
+xcopy /s /q /y "Framework.jar" "D:\itu\S5\Clustering\Sprint\Test_sprint\lib"
 
 REM supp .jar
 rm "Framework.jar"
